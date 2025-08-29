594db58a68f116edab538f3e340828e95282e1e2
Kenny Odukoya <kehindeodukoyaade@gmail.com>
2025-08-29 12:27:52 +0100

    fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "sales-tracker",
+  base: "/sales-tracker/",
 })
 
